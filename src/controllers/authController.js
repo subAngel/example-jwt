544ebbea10c46df1bee6f495f2f4323a2c224c83
subAngel <whoangel.agl@gmail.com>
2023-02-01 14:16:30 -0600

    Signup auth controller completed

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,29 @@
 const { Router } = require("express");
+const debug = require("debug")("jwt:auth");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+const config = require("../config");
 
 const router = new Router();
 
-router.post("/signup", (req, res, next) => {});
+router.post("/signup", async (req, res, next) => {
+	const { username, email, password } = req.body;
+	const user = new User({
+		username,
+		email,
+		password,
+	});
+	user.password = await user.encryptPassword(user.password);
+	await user.save();
+
+	const token = jwt.sign({ id: user._id }, config.secret, {
+		// expiresIn: 60*60*24
+		expiresIn: 60,
+	});
+
+	return res.json({ auth: true, token });
+});
 router.post("/login", (req, res, next) => {
 	res.json("login");
 });
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, Model, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 // Esquema: datos que voy a guardar
 const userSchema = new Schema({
@@ -6,5 +7,10 @@ const userSchema = new Schema({
 	email: String,
 	password: String,
 });
+
+userSchema.methods.encryptPassword = async (password) => {
+	const salt = await bcrypt.genSalt(10);
+	return bcrypt.hash(password, salt);
+};
 
 module.exports = model("User", userSchema);
